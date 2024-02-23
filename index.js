e503899db5326ad4f2a80af2e5e56f7cf66ccad0
Tiara Stinson Lavender <155996098+stinso65@users.noreply.github.com>
2024-02-23 19:02:07 +0000

    reassigned image class and added more js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Add JavaScript below
 window.onload = function() {
-    ScrollReveal().reveal('.gallery', {delay: 1000});
+    ScrollReveal().reveal('.photo1', {delay: 700});
+    ScrollReveal().reveal('.photo2', { delay: 700, origin: 'left' });
+    ScrollReveal().reveal('.photo5', {
+        rotate: {
+            x: 50,
+            z: 50
+        }
+    });
 }

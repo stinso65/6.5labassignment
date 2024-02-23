dd15ed8172f18b3fc7b44934e6acde131310b446
Tiara Stinson Lavender <155996098+stinso65@users.noreply.github.com>
2024-02-23 19:05:29 +0000

    finalized js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Add JavaScript below
 window.onload = function() {
-    ScrollReveal().reveal('.photo1', {delay: 700});
-    ScrollReveal().reveal('.photo2', { delay: 700, origin: 'left' });
+    ScrollReveal().reveal('.photo1', {delay: 600});
+    ScrollReveal().reveal('.photo2', { delay: 800, origin: 'top', distance: '70px' });
+    ScrollReveal().reveal('.photo3', { delay: 700, origin: 'right', distance: '70px' });
+    ScrollReveal().reveal('.photo4', { delay: 700, origin: 'left', distance: '60px' });
     ScrollReveal().reveal('.photo5', {
         rotate: {
             x: 50,
